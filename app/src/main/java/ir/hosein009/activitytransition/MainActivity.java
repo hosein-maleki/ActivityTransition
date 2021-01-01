6c1a0d492c17ce049312b9bc08c77489bd5525bc
hosein maleki <hosein.maleki969@gmail.com>
2021-01-01 15:42:08 +0330

    second commit

--- app/src/main/java/ir/hosein009/activitytransition/MainActivity.java
+++ app/src/main/java/ir/hosein009/activitytransition/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    // hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
