29033be26a37225ae2d4ff0804201bfd5c7239e1
hosein maleki <hosein.maleki969@gmail.com>
2021-01-01 13:42:52 +0330

    first commit

---
+++ app/src/main/java/ir/hosein009/activitytransition/MainActivity.java
@@ -0,0 +1,20 @@
+package ir.hosein009.activitytransition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startActivity(View v){
+        startActivity(new Intent(MainActivity.this,SecondActivity.class));
+    }
+}
---
+++ app/src/main/java/ir/hosein009/activitytransition/ThirdActivity.java
@@ -0,0 +1,14 @@
+package ir.hosein009.activitytransition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
