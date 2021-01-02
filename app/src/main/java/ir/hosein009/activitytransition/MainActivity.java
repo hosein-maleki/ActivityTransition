b30f6ee804458986147fce343d10d91ebca451ff
hosein maleki2 <hosein.maleki979@gmail.com>
2021-01-02 16:07:59 +0330

    add title to MainActivity

--- app/src/main/java/ir/hosein009/activitytransition/MainActivity.java
+++ app/src/main/java/ir/hosein009/activitytransition/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-        getActionBar().setTitle("Hosein maleki");
+        getSupportActionBar().setTitle("Hosein");
     }
 
     public void startActivity(View v){
