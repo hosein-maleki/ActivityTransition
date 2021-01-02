13f423f60c573556105c1bd22e95deb6c8fb23f0
hosein maleki2 <hosein.maleki979@gmail.com>
2021-01-02 15:06:33 +0330

    add ss to gitignore

--- app/src/main/java/ir/hosein009/activitytransition/MainActivity.java
+++ app/src/main/java/ir/hosein009/activitytransition/MainActivity.java
@@ -3,6 +3,7 @@ package ir.hosein009.activitytransition;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setBackgroundColor(Color.BLUE);
     }
 
     public void startActivity(View v){
